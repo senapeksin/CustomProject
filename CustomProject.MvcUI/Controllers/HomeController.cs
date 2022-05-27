020fe03af790bc6bd5dd8fe53ea68ad0c6a22854
SENA <senapksnn@gmail.com>
2022-05-27 13:21:44 +0300

    referance bug fix

---
+++ CustomProject.MvcUI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using CustomProject.Entity;
+using CustomProject.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomProject.MvcUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            CategoriesORM orm = new CategoriesORM();
+            List<Categories> kategoriler = orm.Select();
+            return View(kategoriler);
+        }
+    }
+}
